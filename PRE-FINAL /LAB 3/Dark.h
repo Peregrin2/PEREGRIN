e7077d68775fc8711150e3f0b19043dfb0b0d453
Peregrin <ronellejuneperegrin@gmail.com>
2026-02-28 01:23:34 +0800

    Add Dark class with attack methods

---
+++ PRE-FINAL /LAB 3/Dark.h
@@ -0,0 +1,19 @@
+#ifndef DARK_H
+#define DARK_H
+using namespace std;
+
+class Dark : public Element{
+public:
+
+	 void attack() override {
+		cout << "This Dark is attacking" << endl;
+	}
+
+
+	 void attack(double atk) override {
+		 cout << "Attack power (sqrt): Floor: " << sqrt(floor(atk)) << " Ceil: " << ceil(sqrt(atk)) << endl;
+	}
+
+};
+
+#endif#pragma once
